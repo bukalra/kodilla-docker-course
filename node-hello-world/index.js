bbc67b93586fd7875a964dcd0ded91830bcbb2df
Radek <bukalskiradek@gmail.com>
2023-12-28 11:40:10 +0100

    docker-compose for quotes app

--- node-hello-world/index.js
+++ node-hello-world/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/hello", (req, res) => {
-    res.send("Hello World from Node Application");
+    res.send("Helloooooo World from Node Application - voulume working fine");
 });
 
 app.listen(port, () => {
